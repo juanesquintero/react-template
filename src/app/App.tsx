3ff563807d331c9bdbfe13ae7c58495155ccbbf3
juan_quintero <juan_quintero@epam.com>
2023-02-04 12:21:51 -0500

    chore: relative paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from '@assets/logo.svg';
-import './App.css';
+import '@app/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
