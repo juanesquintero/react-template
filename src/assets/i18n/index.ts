3d13e9d1a90222da6f1d6e649d97c42c99a62bfc
juan_quintero <juan_quintero@epam.com>
2023-02-05 11:20:22 -0500

    chore: add lang detector

--- src/assets/i18n/index.ts
+++ src/assets/i18n/index.ts
@@ -1,19 +1,22 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
 import en from './langs/en';
 import es from './langs/es';
 
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
     .init({
-        resources: { 
-            en: { ...en }, 
-            es: { ...es } 
+        resources: {
+            en: { ...en },
+            es: { ...es }
         },
-        lng: 'es', // if you're using a language detector, do not define the lng option
-        fallbackLng: 'es',
+        fallbackLng: 'en',
         interpolation: {
-            escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+            escapeValue: false,
         },
     });
