57db74473fb9815daea9f67e36df2429141e29dd
juan_quintero <juan_quintero@epam.com>
2023-02-05 20:48:13 -0500

    feat: coverge all Home child component

--- src/app/features/home/Home.tsx
+++ src/app/features/home/Home.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from '@assets/logo.svg';
 import './Home.scss';
-import { Trans, useTranslation } from 'react-i18next';
 import { HomeProps } from './Home.types';
+import Logo from '@src/app/core/logo/Logo';
+import { APP_FILE_PATH } from '@src/app/shared/constants';
+import Link from '@src/app/core/link/Link';
+import Button from '@src/app/core/button/Button';
 
 const Home: React.FC<HomeProps> = (props: HomeProps) => {
-  const { t } = useTranslation();
   return (
     <>
-      <img src={logo} className="home-logo" alt="logo" />
-      <p>
-        <Trans i18nKey='home.edit_and_save' values={{path: props.app?.path}} />
-      </p>
-      <a
-        className="home-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {t('home.learn_react')}
-      </a>
+      <Logo app={{path: APP_FILE_PATH}}></Logo>
+      <Link></Link>
+      <Button></Button>
     </>
   );
 }
