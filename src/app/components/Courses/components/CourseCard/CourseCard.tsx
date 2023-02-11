1af24b6afa7e5e8e32249e18e8e6bc8a86878b2f
juan_quintero <juan_quintero@epam.com>
2023-02-11 01:20:26 -0500

    feat: course card component

--- src/app/components/Courses/components/CourseCard/CourseCard.tsx
+++ src/app/components/Courses/components/CourseCard/CourseCard.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import './CourseCard.scss';
+import { ICourse } from '../../Courses.types';
+import Button from '@src/app/common/Button/Button';
+import formatCreationDate from '@src/app/helpers/formatCreationDate';
+import getCourseDuration from '@src/app/helpers/getCourseDuration';
 
-const App: React.FC = () => {
-	return <section></section>;
+const CourseCard: React.FC<ICourse> = (props: ICourse) => {
+	return (
+		<section className='course-card'>
+			<div>
+				<h5>{props.title}</h5>
+				<p>{props.description}</p>
+			</div>
+
+			<div className='course-card-column'>
+				<div>
+					<strong>Authors:</strong>
+					{props.authors.join(', ')}
+				</div>
+				<div>
+					<strong>Duration:</strong>
+					{getCourseDuration(props.duration)}
+				</div>
+				<div>
+					<strong>Created:</strong>
+					{formatCreationDate(props.creationDate)}
+				</div>
+				<Button text='Show course' style='outline-light' />
+			</div>
+		</section>
+	);
 };
 
-export default App;
+export default CourseCard;
