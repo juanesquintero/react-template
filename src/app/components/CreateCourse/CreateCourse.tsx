609a1cde59528cb8b3fbcc0736672e81924df0d2
juan_quintero <juan_quintero@epam.com>
2023-02-13 10:02:26 -0500

    feat: create course  form

--- src/app/components/CreateCourse/CreateCourse.tsx
+++ src/app/components/CreateCourse/CreateCourse.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import '@app/App.scss';
 import Input from '@src/app/common/Input/Input';
 import Button from '@src/app/common/Button/Button';
+import AuthorsForm from './components/AutorsForm/AuthorsForm';
+import getCourseDuration from '@src/app/helpers/getCourseDuration';
 
 const CreateCourse: React.FC = () => {
 	return (
-		<form className='bg-white rounded p-5'>
-			<div className='form-row'>
-				<div className='form-inline'>
-					<label>Title</label>
-					<br />
-					<Input
-						type='text'
-						placeholder='Python Course No. 1'
-						size='sm'
-						inline={true}
-						className='mr-auto'
-					/>
-
-					<Button
-						type='submit'
-						text='Create course'
-						icon='plus'
-						style='outline-dark'
-					/>
-				</div>
+		<>
+			<h5>Create Course</h5>
+			<form className='bg-white rounded px-5 py-3'>
 				<div className='form-group'>
-					<label>Description</label>
-					<Input type='textarea' placeholder='Password' />
+					<div className='form-inline d-flex justify-content-between'>
+						<Input
+							label='Title'
+							type='text'
+							placeholder='Python Course No. 1'
+						/>
+
+						<div className='d-flex align-items-baseline'>
+							<Button
+								type='submit'
+								text='Create course'
+								icon='plus'
+								style='outline-dark'
+							/>
+						</div>
+					</div>
+					<div className='form-group'>
+						<Input label='Description' type='textarea' placeholder='Password' />
+					</div>
+					<div className='form-group d-flex align-items-center'>
+						<Input label='Duration' type='number' />
+						<p className='mt-5 ms-3'>{getCourseDuration(100)}</p>
+					</div>
 				</div>
-			</div>
-		</form>
+				<AuthorsForm />
+			</form>
+		</>
 	);
 };
 
