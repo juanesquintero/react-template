affa5b5ceb715ef96169c1d2a7801aa2669928e2
juan_quintero <juan_quintero@epam.com>
2023-02-12 22:25:27 -0500

    refactor: input types

--- src/app/common/Input/Input.tsx
+++ src/app/common/Input/Input.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { IInputProps } from './Input.types';
 
-const Input: React.FC<any> = (props: any) => {
-	const propsCopy = {
-		...props,
-		className: props.className + ' form-control',
+const Input: React.FC<IInputProps> = (props: IInputProps) => {
+	const { label, style, size, ..._props } = props;
+
+	_props.className = 'form-control m-1';
+	if (style) {
+		_props.className += ' btn-' + style;
+	}
+	if (size) {
+		_props.className += ' btn-' + size;
+	}
+
+	const renderInput = (type: string) => {
+		const inputTag = {
+			textarea: <textarea rows={3} {..._props}></textarea>,
+			select: <select {..._props} />,
+		};
+		if (inputTag[type]) {
+			delete _props.type;
+			return inputTag[type];
+		}
+		return <input {..._props} />;
 	};
-	return <input {...propsCopy} />;
+
+	return (
+		<>
+			{label ? <label>{label}</label> : null}
+			{renderInput(props.type)}
+		</>
+	);
 };
 
 export default Input;
---
+++ src/app/common/Input/Input.types.ts
@@ -0,0 +1,43 @@
+export interface IInputProps {
+	label?: string;
+	placeholder?: string;
+	className?: string;
+	onChange?: () => void;
+	style?: style;
+	type?: customInputType | inputType;
+	size?: 'sm' | 'lg';
+	children?: JSX.Element[] | JSX.Element;
+}
+
+export interface IInputAtrr extends IInputProps {
+	className?: string;
+}
+
+type style = 'form-check' | 'form-control' | 'form-control-file';
+
+type customInputType = 'textarea' | 'select';
+
+type inputType =
+	| 'text'
+	| 'button'
+	| 'checkbox'
+	| 'color'
+	| 'date'
+	| 'datetime-local'
+	| 'email'
+	| 'file'
+	| 'hidden'
+	| 'image'
+	| 'month'
+	| 'number'
+	| 'password'
+	| 'radio'
+	| 'range'
+	| 'reset'
+	| 'search'
+	| 'submit'
+	| 'tel'
+	| 'text'
+	| 'time'
+	| 'url'
+	| 'week';
